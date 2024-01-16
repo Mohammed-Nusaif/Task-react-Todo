fdf2ccd007126cda6a0696c3825dc2f0aa0fccc1
Mohammed-Nusaif <nusaiffanukp@gmail.com>
2024-01-16 16:35:51 +0530

    first commit

---
+++ src/components/Todolist.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from "uuid";
+import Todo from './Todo'
+import Edit from './Edit'
+import Addto from './Addto';
+
+
+function Todolist() {
+    const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+  }
+
+  const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  }
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  }
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+  return (
+    <div>
+         <div className="TodoWrapper">
+      <h1>Get Things Done !</h1>
+      <Addto addTodo={addTodo} />
+      {/* display todos */}
+      {todos.map((todo) =>
+        todo.isEditing ? (
+          <Edit editTodo={editTask} task={todo} />
+        ) : (
+          <Todo
+            key={todo.id}
+            task={todo}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+            toggleComplete={toggleComplete}
+          />
+        )
+      )}
+    </div>
+    </div>
+  )
+}
+
+export default Todolist
---
+++ src/components/Todolocal.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import Edit from './Edit';
+import Addto from './Addto';
+import Todo from './Todo';
+import { v4 as uuidv4 } from 'uuid';
+
+function Todolocal() {
+    const [todos, setTodos] = useState([])
+
+    useEffect(() => {
+        const savedTodos = JSON.parse(localStorage.getItem('todos')) || [];
+        setTodos(savedTodos);
+    }, []);
+
+    const addTodo = todo => {
+        const newTodos = [...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}];
+        setTodos(newTodos);
+        localStorage.setItem('todos', JSON.stringify(newTodos));
+    }
+
+    const toggleComplete = id => {
+        const newTodos = todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo);
+        setTodos(newTodos);
+        localStorage.setItem('todos', JSON.stringify(newTodos));
+    }
+
+    const deleteTodo = id => {
+        const newTodos = todos.filter(todo => todo.id !== id);
+        setTodos(newTodos);
+        localStorage.setItem('todos', JSON.stringify(newTodos));
+    }
+
+    const editTodo = id => {
+        setTodos(todos.map(todo => todo.id === id ? {...todo, isEditing: !todo.isEditing} : todo))
+    }
+
+    const editTask = (task, id) => {
+        const newTodos = todos.map(todo => todo.id === id ? {...todo, task, isEditing: !todo.isEditing} : todo);
+        setTodos(newTodos);
+        localStorage.setItem('todos', JSON.stringify(newTodos));
+    }
+  return (
+    <div>
+              <h1>Get Things Done!</h1>
+        <Addto addTodo={addTodo} />
+        {todos.map((todo, index) => (
+            todo.isEditing ? (
+                <Edit editTodo={editTask} task={todo} />
+            ) : (
+                <Todo task={todo} key={index} toggleComplete={toggleComplete} deleteTodo={deleteTodo} editTodo={editTodo} />
+            )
+            
+        ))}
+         
+    </div>
+  )
+}
+
+export default Todolocal
